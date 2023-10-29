875d021e53e79e45006e3165ccc1202a74427645
SungChul-CHA <sungchulCHA13@gmail.com>
2023-10-29 13:48:51 +0900

    add uart_rx

--- HW2.srcs/sources_1/new/uart_rx.v
+++ HW2.srcs/sources_1/new/uart_rx.v
@@ -20,7 +20,55 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 
-module uart_rx(
-
+module uart_rx (
+    input clk,
+    input rst,
+    input uart_rxd, 	    //수신 데이터
+    output reg rx_busy,	    //idle 상태가 아닐때 '1' 
+    output reg [7:0] uart_rx_data //수신한 8비트 data
     );
+
+    reg [3:0] c_state, n_state;
+    reg en;
+    
+    parameter IDLE_ST = 4'd0, START_ST = 4'd1, BIT0_ST = 4'd2,
+    BIT1_ST = 4'd3, BIT2_ST = 4'd4, BIT3_ST =4'd5, BIT4_ST = 4'd6,
+    BIT5_ST = 4'd7, BIT6_ST = 4'd8, BIT7_ST = 4'd9, STOP_ST = 4'd10,
+    BAUD_RATE = 115200, DATA_BIT = 8, PARITY_BIT = 0, STOP_BIT = 1;
+    
+    always @ (*) begin
+        case (c_state)
+            IDLE_ST: begin 
+                rx_busy = 0;
+                en = 0;
+                n_state = START_ST;
+            end
+            START_ST: begin rx_busy = 1; n_state = BIT0_ST; end
+            BIT0_ST: begin uart_rx_data[0] = uart_rxd; n_state = BIT1_ST; end
+            BIT1_ST: begin uart_rx_data[1] = uart_rxd; n_state = BIT2_ST; end
+            BIT2_ST: begin uart_rx_data[2] = uart_rxd; n_state = BIT3_ST; end
+            BIT3_ST: begin uart_rx_data[3] = uart_rxd; n_state = BIT4_ST; end
+            BIT4_ST: begin uart_rx_data[4] = uart_rxd; n_state = BIT5_ST; end
+            BIT5_ST: begin uart_rx_data[5] = uart_rxd; n_state = BIT6_ST; end
+            BIT6_ST: begin uart_rx_data[6] = uart_rxd; n_state = BIT7_ST; end
+            BIT7_ST: begin uart_rx_data[7] = uart_rxd; n_state = STOP_ST; end
+            STOP_ST: begin n_state = IDLE_ST; end
+            default: n_state = IDLE_ST;
+        endcase
+    end
+    
+    always @ (posedge clk_b, posedge rst) begin
+        if (rst) c_state <= 0;
+        else if (clk_b & ~c_state) c_state <= n_state;
+    end
+    
+    always @ (negedge uart_rxd) begin
+        if (c_state == IDLE_ST) begin
+            en <= 1;
+            c_state <= START_ST;
+        end
+    end
+
+    gen_counter_en #(868) gen_cnt_en_inst (clk, rst, en, clk_b);
+
 endmodule
